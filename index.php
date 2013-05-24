d88d840ab44b32e987123c61abd2334e39b4608a
Akhmad Zaini <zaini1983@gmail.com>
2013-05-24 15:41:49 +0700

    Persiapan aset dan template 1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Alef:700' rel='stylesheet' type='text/css'>
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+		<link href="assets/css/zmr.css" rel="stylesheet">
+		<script src="assets/js/jquery-1.9.1.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<title>Template ZMR</title>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="cointainer">
+				<div class="row-fluid">
+					<div class="span8 pull-right">
+						<h1 class="alef">Z - Medical Record System</h1>
+						<p class="lead no-space">Sistem Informasi Rekam Medis</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
